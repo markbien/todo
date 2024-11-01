b6e517ec9f3b90a121c3d7f9eaf7c802c4ed106f
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-02 02:31:02 +0800

    Configure webpack to recognize and use html and css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,5 +7,23 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todoodoo'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
