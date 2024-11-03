76d907ae821d90158a3c5f989d04d97bf037415d
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-04 06:00:48 +0800

    Modify createCard function to accept newId

--- src/modules/dom/main-div.js
+++ src/modules/dom/main-div.js
@@ -1,4 +1,6 @@
-import { createDiv } from "./dom-elements"
+import { createDiv, createLi, createPara } from "./dom-elements";
+import editIcon from '../../images/edit-colored.png';
+import trashIcon from '../../images/trash-colored.png';
 
 export default function createMainDiv(month, day, time){
   const mainDiv = createDiv('main');
@@ -67,3 +69,31 @@ function createMainContainer(){
   return mainContainerDiv;
 }
 
+export function createCard(newId, newTitle, newDescription, newDueDate){
+  const card = createLi('card');
+  card.dataset.todoId = newId;
+
+  const leftDiv = createDiv('left');
+  const todoDetailsDiv = createDiv('todo-details');
+  const title = createPara('title', newTitle);
+  const description = createPara('description', newDescription);
+  const dueDate = createPara('due-date', "");
+  dueDate.innerHTML = `Due: <span class='due-date'>${newDueDate}</span>`;
+  leftDiv.appendChild(todoDetailsDiv);
+  todoDetailsDiv.appendChild(title);
+  todoDetailsDiv.appendChild(description);
+  todoDetailsDiv.appendChild(dueDate);
+
+  const rightDiv = createDiv('right');
+  const edit = document.createElement('img');
+  edit.src = editIcon;
+  const trash = document.createElement('img');
+  trash.src = trashIcon;
+  rightDiv.appendChild(edit);
+  rightDiv.appendChild(trash);
+
+  card.appendChild(leftDiv);
+  card.appendChild(rightDiv);
+
+  return card;
+}
