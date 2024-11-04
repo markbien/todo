ae833f13db6ab7ac1e5ee2461f066129d4f12872
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-05 03:47:08 +0800

    Modify findProjectIndex and add mapProjectName

--- src/modules/project-collection.js
+++ src/modules/project-collection.js
@@ -1,28 +1,39 @@
 export default function projectCollection() {
   const collection = [];
   const addProject = project => collection.push(project);
-  const findProjectIndex = index => {
+
+  const findProjectIndex = projectName => {
     return collection.findIndex(project => {
-      return project.getId() === index;
+      return project.getId() === projectName;
     });
   };
+
   const removeProject = index => {
     collection.splice(index, 1);
   }
+
   const printProjectDetails = () =>{
     return collection.forEach(project => {
       project.printDetails();
     });
   }
+
+  const mapProjectName = () => {
+    return collection.map(project => {
+      return project.getId();
+    });
+  }
+
   const getProject = index => {
     return collection[index];
-  }
+  };
 
   return {
     addProject,
     findProjectIndex,
     removeProject,
     printProjectDetails,
+    mapProjectName,
     getProject,
   }
 };
