93e309f9581fff0ab0edcfe7789abcd77067a8e5
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-05 04:08:29 +0800

    Make projectCollection accessible outside for other functions, and create generateId function

--- src/index.js
+++ src/index.js
@@ -4,82 +4,31 @@ import { createAddProject } from "./modules/dom/div-input.js";
 import createProjectCollection from "./modules/project-collection.js";
 import project from "./modules/project.js";
 import { createDefaultProject, addProjectToNavInDOM } from "./modules/dom/nav.js";
-// import todo from './modules/todo.js';
-// import project from './modules/project.js';
-// import projectCollection from './modules/project-collection.js';
-
-// const collection = projectCollection();
-
-// const workProject = project(1, "Work");
-// const task1 = todo(12345, "Restart laptop", "Restart laptop to speed up performance again", "Saturday");
-// const task2 = todo(12346, "Shutdown laptop", "Shut down laptop during weekends", "Saturday");
-// workProject.addATodo(task1);
-// workProject.addATodo(task2);
-
-// const homeProject = project(2, "Home");
-// const task3 = todo(12347, "Clean aircon", "Clean aircon every week to prevent clogging", "Sunday");
-// const task4 = todo(12348, "Remove molds", "Molds can accumulate due to rainy season and moist", "Sunday");
-// homeProject.addATodo(task3);
-// homeProject.addATodo(task4);
-
-// collection.addProject(workProject);
-// collection.addProject(homeProject);
-
-// collection.printProjectDetails();
-// workProject.printTodos();
-// homeProject.printTodos();
-
-// const shutdownIndex = workProject.getTodoIndex(12346);
-// console.log(shutdownIndex);
-// workProject.removeATodo(workProject.getTodoIndex(12346));
-// workProject.printTodos();
-// workProject.removeATodo(0);
-// workProject.removeATodo(workProject.getTodoIndex(12346));
-// console.log("-----");
-// workProject.printTodos();
-// console.log("-----");
-
-// collection.printProjectDetails();
-// collection.removeProject(0);
-// collection.printProjectDetails();
-// collection.removeProject(1);
-// console.log("----")
-// collection.printProjectDetails();
-
-// import { createAddProject } from "./modules/dom/div-input";
-
-// const ta = createAddDetails();
-// document.body.appendChild(ta);
-
-// const ul = document.querySelector('.project-container > ul');
-// const addProject = createAddProject();
-// ul.appendChild(addProject);;
 
 initializeWebsiteDom();
+const projectCollection = initializeStorage();
+
+loadDefaultProject(projectCollection) // Load default project here
 
 function initializeStorage(){
-  const projectCollection = createProjectCollection();
-  loadDefaultProject(projectCollection);
-
-  return projectCollection;
+  // this is where we can load from JSON later
+  return createProjectCollection();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   addEventListenerToNewProject();
-  // Load projects here
-  initializeStorage();
 });
 
 function loadDefaultProject(projectCollection){
-  const DEFAULT_PROJECT_INDEX = 0;
-  const defaultIndex = projectCollection.findProjectIndex('Default');
+  let defaultIndex = projectCollection.findProjectIndex('default');
   if (defaultIndex === -1) { // Create "Default" project
-    const defaultProject = project(DEFAULT_PROJECT_INDEX, "Default");
+    const defaultProject = project(generateId("Default"), "Default");
     projectCollection.addProject(defaultProject);
+    defaultIndex = projectCollection.findProjectIndex('default');
   }
 
   // Get from storage > Add Default project to DOM
-  const defaultProject = projectCollection.getProject(DEFAULT_PROJECT_INDEX);
+  const defaultProject = projectCollection.getProject(defaultIndex);
   const defaultProjectDOM = createDefaultProject(defaultProject.getId(), defaultProject.getName());
   addProjectToNavInDOM(defaultProjectDOM);
 }
@@ -115,12 +64,20 @@ function attachEventSaveProject() {
   const saveBtn = document.querySelector("#save-new-project");
   saveBtn.addEventListener("click", function () {
     // Add project here
+    const projectName = document.querySelector('#project-name');
+    saveNewProjectToCollection(projectName.value);
 
     removeAddProjectDiv();
   });
 }
 
-function generateRandomIdFrom1(maxNumber){
-  const randNum = Math.floor(Math.random() * maxNumber) + 1; // +1 to exclude 0 from results
-  return randNum;
+function saveNewProjectToCollection(projectName){
+  const newId = generateId(projectName);  
+  const newProject = project(newId, projectName);
+  projectCollection.addProject(newProject); // Add project to collection
+  projectCollection.printProjectDetails();
+}
+
+function generateId(projectName) {
+  return projectName.replaceAll(' ', '').toLowerCase();
 }
