df0f65cc480f4642e4b1dd80e29df72e4a445816
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-05 08:25:49 +0800

    Add storage.js to export and import data from localStorage

---
+++ src/modules/storage.js
@@ -0,0 +1,42 @@
+import project from "./project";
+
+export function getDataFromStorage(){
+
+}
+
+export function importDataFromStorage(storage){
+  checkThatStorageExists();
+  let projectCollection = JSON.parse(localStorage.getItem('projectCollection'));
+  checkThatDefaultProjectExists(projectCollection);
+  projectCollection.forEach(item => {
+    storage.addProject(project(item.id, item.name));
+  });
+
+  console.log(projectCollection);
+  return projectCollection;
+}
+
+export function exportDataToStorage(storage){
+  localStorage.setItem("projectCollection", storage.convertToJSON());
+}
+
+function checkThatStorageExists(){
+  let projectCollection = JSON.parse(localStorage.getItem('projectCollection'));
+  if (projectCollection === null) {
+    localStorage.setItem('projectCollection', '[]');
+  }
+}
+
+function checkThatDefaultProjectExists(projectCollection){
+  const index = projectCollection.findIndex(item => {
+    return item.id === 'default';
+  });
+
+  if (index !== -1) return;
+  const defaultProject = {
+    id: 'default',
+    name: 'Default',
+  };
+
+  projectCollection.unshift(defaultProject);
+}
