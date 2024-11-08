81f377aa491b5cf8216c19afe7811c77ddabd403
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-09 05:56:42 +0800

    Use format to get the month and day

--- src/modules/dom/landing-page.js
+++ src/modules/dom/landing-page.js
@@ -1,23 +1,15 @@
 import createNav from "./nav";
-import createMainDiv, { createCard } from "./main-div";
+import createMainDiv from "./main-div";
+import { format } from "date-fns";
+
+const date = new Date();
+const month = format(date, "MMM");
+const day = format(date, "d");
 
 const nav = createNav();
-const main = createMainDiv("Nov", "4", "Morning");
+const main = createMainDiv(month, day, "Day");
 
 export default function initializeWebsiteDom() {
   document.body.appendChild(nav);
   document.body.appendChild(main);
 }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   // const cardContainer = document.querySelector(".card-container");
-
-//   const card1 = createCard(
-//     12345,
-//     "Clean aircon",
-//     "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio veritatis enim minus? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio veritatis enim minus? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio veritatis enim minus?",
-//     "2024-12-06"
-//   );
-  
-//   // cardContainer.appendChild(card1);
-// });
