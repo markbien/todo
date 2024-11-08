5665be69dcb9d313678b854bace818a00da8d677
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-08 15:44:42 +0800

    Allow todos to load from each projects via storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./style.css";
 import initializeWebsiteDom from "./modules/dom/landing-page.js";
-import { createAddProject, createAddDetails, createEditDetails } from "./modules/dom/div-input.js";
+import {
+  createAddProject,
+  createAddDetails,
+  createEditDetails,
+} from "./modules/dom/div-input.js";
 import createProjectCollection from "./modules/project-collection.js";
 import project from "./modules/project.js";
 import {
@@ -14,7 +18,7 @@ import {
   exportDataToStorage,
 } from "./modules/storage.js";
 import todo from "./modules/todo.js";
-import {createCard} from './modules/dom/main-div.js';
+import { createCard } from "./modules/dom/main-div.js";
 
 initializeWebsiteDom();
 const projectCollection = initializeStorage();
@@ -84,7 +88,7 @@ function saveNewProjectToCollection(projectName) {
 
   const newProject = project(newId, projectName);
   projectCollection.addProject(newProject); // Add project to collection
-  projectCollection.printProjectDetails();
+  // projectCollection.printProjectDetails();
 
   const newProjectDOM = createProjectDom(newId, projectName);
   addProjectToNavInDOM(newProjectDOM); // Add project to DOM, will still need to add eventlisteners here
@@ -106,112 +110,186 @@ function generateId(projectName) {
 }
 
 function loadProjects(projectCollection) {
-  projectCollection.mapProjectNameAndId().forEach((project) => {
-    if (project.id === "default") {
-      const defaultProject = createDefaultProject(project.id, project.name);
+  projectCollection.printAll().forEach((newProject) => {
+    if (newProject.id === "default") {
+      const defaultProject = createDefaultProject(
+        newProject.id,
+        newProject.name
+      );
       addProjectToNavInDOM(defaultProject);
-      defaultProject.addEventListener("click", function () {
-        loadTodos(project.id, this);
+
+      const cardContainer = document.querySelector(".card-container");
+      cardContainer.textContent = "";
+
+      // This is to load the Default's todos first
+      newProject.todos.forEach(() => {
+        loadTodos(newProject.id);
+      });
+
+      defaultProject.addEventListener("click", () => {
+        clearActiveProject();
+
+        document.querySelector(".project-container").dataset.activeProject =
+          "default";
+        document
+          .querySelector("[data-project-id='default']")
+          .classList.add("active");
+        loadTodos(newProject.id);
       });
     } else {
-      const newProjectDOM = createProjectDom(project.id, project.name);
+      const newProjectDOM = createProjectDom(newProject.id, newProject.name);
       addProjectToNavInDOM(newProjectDOM);
-      addEventToDeleteProject(newProjectDOM, project.id);
-      addEventToEditProject(newProjectDOM);
 
       newProjectDOM.addEventListener("click", function () {
-        loadTodos(project.id, this);
+        clearActiveProject();
+        document.querySelector(".project-container").dataset.activeProject =
+          newProject.id;
+        document
+          .querySelector(`[data-project-id='${newProject.id}']`)
+          .classList.add("active");
+
+        clearCardContainer();
+        newProject.todos.forEach(() => {
+          loadTodos(newProject.id);
+        });
       });
+
+      addEventToDeleteProject(newProjectDOM, newProject.id);
+      addEventToEditProject(newProjectDOM);
     }
   });
 }
 
-function loadTodos(projectId, currentDomSelected) {
+function clearActiveProject() {
   const projectContainer = document.querySelector(".project-container");
-  projectContainer.dataset.activeProject = projectId;
-
   const projects = projectContainer.firstChild.childNodes;
   projects.forEach((project) => {
     project.classList.remove("active");
   });
-  currentDomSelected.classList.add("active");
+}
 
-
+function loadTodos(projectId) {
   clearCardContainer();
 
   const projectIndex = projectCollection.findProjectIndex(projectId);
   const todoArr = projectCollection.getProject(projectIndex).getTodos();
-  todoArr.forEach(todo => {
-    const newCard = createCard(todo.id, todo.title, todo.description, todo.dueDate, todo.completionStatus);
-    if (todo.completionStatus === 'true') {
+  todoArr.forEach((todo) => {
+    const newCard = createCard(
+      todo.id,
+      todo.title,
+      todo.description,
+      todo.dueDate,
+      todo.completionStatus
+    );
+    if (todo.completionStatus === "true") {
       const todoDom = newCard.firstChild.firstChild;
-      todoDom.childNodes[0].classList.add('completed');
-      todoDom.childNodes[1].classList.add('completed');
-      todoDom.childNodes[2].classList.add('completed');
+      todoDom.childNodes[0].classList.add("completed");
+      todoDom.childNodes[1].classList.add("completed");
+      todoDom.childNodes[2].classList.add("completed");
     }
 
-    const cardContainer = document.querySelector('.card-container');
+    const cardContainer = document.querySelector(".card-container");
     cardContainer.appendChild(newCard);
 
-    const deleteImg = newCard.lastChild.lastChild
-    addEventToDeleteATodo(deleteImg, projectIndex, todo.id, cardContainer, newCard);
+    const deleteImg = newCard.lastChild.lastChild;
+    addEventToDeleteATodo(
+      deleteImg,
+      projectIndex,
+      todo.id,
+      cardContainer,
+      newCard
+    );
 
     const editImg = newCard.lastChild.firstChild;
-    addEventToEditATodo(editImg, todo.title, todo.description, todo.dueDate, todo.completionStatus);
+    addEventToEditATodo(
+      editImg,
+      todo.title,
+      todo.description,
+      todo.dueDate,
+      todo.completionStatus
+    );
   });
 }
 
-function addEventToDeleteATodo(img, projectIndex, todoId, cardContainer, newCard){
-  img.addEventListener('click', function(){
-    const currentTodoIndex = projectCollection.getProject(projectIndex).getTodoIndexById(todoId);
+function addEventToDeleteATodo(
+  img,
+  projectIndex,
+  todoId,
+  cardContainer,
+  newCard
+) {
+  img.addEventListener("click", function () {
+    const currentTodoIndex = projectCollection
+      .getProject(projectIndex)
+      .getTodoIndexById(todoId);
     projectCollection.getProject(projectIndex).removeATodo(currentTodoIndex);
     cardContainer.removeChild(newCard);
+    exportDataToStorage(projectCollection);
   });
 }
 
-function addEventToEditATodo(img, currentTitle, currentDescription, currentDueDate, currentStatus){
-  img.addEventListener('click', function(){
-    if (document.body.lastChild.classList.contains('add-details')) return; // Ensure no duplicate edit window appears
-    const editDetails = createEditDetails(currentTitle, currentDescription, currentDueDate, currentStatus);
-    const todoFromStorageCompletionStatus = img.parentElement.parentElement.dataset.completionStatus;
-    if (todoFromStorageCompletionStatus === 'true') {
+function addEventToEditATodo(
+  img,
+  currentTitle,
+  currentDescription,
+  currentDueDate,
+  currentStatus
+) {
+  img.addEventListener("click", function () {
+    if (document.body.lastChild.classList.contains("add-details")) return; // Ensure no duplicate edit window appears
+    const editDetails = createEditDetails(
+      currentTitle,
+      currentDescription,
+      currentDueDate,
+      currentStatus
+    );
+    const todoFromStorageCompletionStatus =
+      img.parentElement.parentElement.dataset.completionStatus;
+    if (todoFromStorageCompletionStatus === "true") {
       editDetails.lastChild.childNodes[3].childNodes[1].checked = true;
     }
 
     document.body.appendChild(editDetails);
 
     const saveChangesBtn = editDetails.lastChild.lastChild;
-    saveChangesBtn.addEventListener('click', ()=> {
+    saveChangesBtn.addEventListener("click", () => {
       const todo = img.parentElement.parentElement;
       const todoId = todo.dataset.todoId;
       const newTitle = editDetails.lastChild.childNodes[0].lastChild.value;
-      const newDescription = editDetails.lastChild.childNodes[1].lastChild.value;
+      const newDescription =
+        editDetails.lastChild.childNodes[1].lastChild.value;
       const newDueDate = editDetails.lastChild.childNodes[2].lastChild.value;
-      const newStatus = document.querySelector('input[name="status"]:checked').getAttribute('id');
+      const newStatus = document
+        .querySelector('input[name="status"]:checked')
+        .getAttribute("id");
 
-      const projectId = document.querySelector('.project-container').dataset.activeProject;
+      const projectId =
+        document.querySelector(".project-container").dataset.activeProject;
       const projectIndex = projectCollection.findProjectIndex(projectId);
-      const currentTodo = projectCollection.getProject(projectIndex).getTodo(todoId);
+      const currentTodo = projectCollection
+        .getProject(projectIndex)
+        .getTodo(todoId);
       currentTodo.setTitle(newTitle);
       currentTodo.setDescription(newDescription);
       currentTodo.setDueDate(newDueDate);
       currentTodo.setStatus(newStatus);
-      
+
       todo.firstChild.firstChild.childNodes[0].textContent = newTitle;
       todo.firstChild.firstChild.childNodes[1].textContent = newDescription;
       todo.firstChild.firstChild.childNodes[2].textContent = newDueDate;
       todo.dataset.completionStatus = newStatus;
-      
-      if (newStatus === 'true') {
-        todo.firstChild.firstChild.childNodes[0].classList.add('completed');
-        todo.firstChild.firstChild.childNodes[1].classList.add('completed');
-        todo.firstChild.firstChild.childNodes[2].classList.add('completed');
+
+      if (newStatus === "true") {
+        todo.firstChild.firstChild.childNodes[0].classList.add("completed");
+        todo.firstChild.firstChild.childNodes[1].classList.add("completed");
+        todo.firstChild.firstChild.childNodes[2].classList.add("completed");
       } else {
-        todo.firstChild.firstChild.childNodes[0].classList.remove('completed');
-        todo.firstChild.firstChild.childNodes[1].classList.remove('completed');
-        todo.firstChild.firstChild.childNodes[2].classList.remove('completed');
+        todo.firstChild.firstChild.childNodes[0].classList.remove("completed");
+        todo.firstChild.firstChild.childNodes[1].classList.remove("completed");
+        todo.firstChild.firstChild.childNodes[2].classList.remove("completed");
       }
 
+      exportDataToStorage(projectCollection);
       document.body.removeChild(editDetails);
     });
   });
@@ -302,39 +380,47 @@ addTodoBtn.addEventListener("click", function () {
 
   const addTodoBtn = addTodoWindow.lastChild.lastChild;
   addTodoBtn.addEventListener("click", function () {
-    const title = document.querySelector('#title').value
+    const title = document.querySelector("#title").value;
     const id = generateId(title);
-    const description = document.querySelector('#description').value;
-    const dueDate = document.querySelector('#due-date').value;
+    const description = document.querySelector("#description").value;
+    const dueDate = document.querySelector("#due-date").value;
 
     // Make control statement to ensure that all fields are completed
 
     const newTodo = todo(id, title, description, dueDate);
-    
+
     projectCollection.getProject(currentProjectIndex).addATodo(newTodo);
-    
+
     // load todo in page
-    const cardContainer = document.querySelector('.card-container');
-    const newCard = createCard(newTodo.getId(), newTodo.getTitle(), newTodo.getDescription(), newTodo.getDueDate(), false);
+    const cardContainer = document.querySelector(".card-container");
+    const newCard = createCard(
+      newTodo.getId(),
+      newTodo.getTitle(),
+      newTodo.getDescription(),
+      newTodo.getDueDate(),
+      false
+    );
     cardContainer.appendChild(newCard);
 
     const deleteImg = newCard.lastChild.lastChild;
     const editImg = newCard.lastChild.firstChild;
     const projectIndex = projectCollection.findProjectIndex(currentProjectId);
-    
+
     addEventToDeleteATodo(deleteImg, projectIndex, id, cardContainer, newCard);
     addEventToEditATodo(editImg, title, description, dueDate, false);
+
+    exportDataToStorage(projectCollection);
 
     document.body.removeChild(addTodoWindow);
   });
 });
 
-document.querySelector('.logo').addEventListener('click', function(){
-  // projectCollection.getProject(0).printTodos();
-  console.log(projectCollection.getProject(0).getTodos());
+document.querySelector(".logo").addEventListener("click", function () {
+  // console.log(projectCollection.getProject(0).getTodos());
+  console.log(projectCollection.printAll());
 });
 
-function clearCardContainer(){
-  const cardContainer = document.querySelector('.card-container');
-  cardContainer.textContent = '';
+function clearCardContainer() {
+  const cardContainer = document.querySelector(".card-container");
+  cardContainer.textContent = "";
 }
