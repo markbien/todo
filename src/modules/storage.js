13837c020c93451514a733ec00ebf943bf15751e
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-11-08 15:43:59 +0800

    Modify importDataFromStorage function

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,11 +1,23 @@
 import project from "./project";
+import todo from "./todo";
 
 export function importDataFromStorage(storage){
   checkThatStorageExists();
   let projectCollection = JSON.parse(localStorage.getItem('projectCollection'));
   checkThatDefaultProjectExists(projectCollection);
-  projectCollection.forEach(item => {
-    storage.addProject(project(item.id, item.name));
+
+  projectCollection.forEach(proj => {
+    const newProject = project(proj.id, proj.name);
+
+    if (proj.todos !== undefined) {
+      proj.todos.forEach(myTodo => {
+        const newTodo = todo(myTodo.id, myTodo.title, myTodo.description, myTodo.dueDate)
+        if (myTodo.completionStatus === 'true') newTodo.setStatus('true');
+        newProject.addATodo(newTodo);
+      });
+    }
+
+    storage.addProject(newProject);
   });
 
   return projectCollection;
